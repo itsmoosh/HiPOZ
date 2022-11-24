7f5a07ac5237cb970dc68fd61934cd0aa43bc0c3
Marshall Styczinski <itsmoosh@gmail.com>
2022-11-23 21:12:14 -0800

    Changed int cast to round for uS/cm label

--- gamryTools.py
+++ gamryTools.py
@@ -100,7 +100,7 @@ class Solution:
             else:
                 self.sigmaStd_Sm = np.nan
             self.legLabel = f'{self.sigmaStd_Sm:.4f}'
-            self.lbl_uScm = int(self.sigmaStd_Sm*1e4)
+            self.lbl_uScm = round(self.sigmaStd_Sm*1e4)
 
         self.color = self.cmap(np.log(self.lbl_uScm)/np.log(80000))
         self.fitColor = LightenColor(self.color, lightnessMult=0.4)
