9889a86fe8e5a968fe3075e58a59c71b19ee0861
vancesteven <vance.steven@gmail.com>
2024-05-06 04:18:03 -0700

    simplified analysis in gamryTools to do a decent job with CPE fits to select NaCl data. Further optimization may be needed, but the approach fits impedances to better than 10% and often better than 1%. data in the allMeas array of Solution objects are now packaged into a TimeSeries object and send to DataSelector.py

--- gamry_HiP.py
+++ gamry_HiP.py
@@ -4,8 +4,11 @@ from glob import glob
 import logging
 from datetime import datetime as dtime
 
-from gamryTools import Solution, CalStdFit
-from gamryPlots import PlotY, PlotZ, PlotZvsf, PlotPhasevsf, PlotZfit, PlotSigma
+from gamryTools import Solution, CalStdFit, TimeSeries
+from gamryPlots import PlotY, PlotZ, PlotZvsf, PlotPhasevsf, PlotZfit, PlotTimeseries
+
+from PyQt5.QtWidgets import QApplication
+from DataSelector import DataSelector
 
 from matplotlib.cm import get_cmap
 import matplotlib.pyplot as plt
@@ -50,96 +53,53 @@ figSize = (6,6)
 xtn = 'png'
 PLOT_AIR = False
 
-# Import data
-add = None
-nDates = np.size(dates)
-allMeas = np.empty(nDates,dtype=object)
-for d_ind,thisDate in enumerate(dates):
-    pathHead = os.path.join(os.path.join('data', thisDate))
-    gamryFiles = glob(os.path.join(pathHead, 'Conductivity*', '*.txt'))
-    # gamryFiles = [f for f in fList if re.search(thisDate+'-'+'[0-9][0-9][0-9][0-9]_', f)]
-    nSweeps = np.size(gamryFiles)
-    meas = np.empty(nSweeps, dtype=object)
-    calStd = CalStdFit(interpMethod='cubic')
+def main():
+    app = QApplication(sys.argv)  # Ensure QApplication is initialized
+    # Import data
+    add = None
+    nDates = np.size(dates)
+    allMeas = np.empty(nDates,dtype=object)
+    for d_ind,thisDate in enumerate(dates):
+        print(f'Processing {thisDate}/')
+        pathHead = os.path.join(os.path.join('data', thisDate))
+        gamryFiles = glob(os.path.join(pathHead, 'Conductivity*', '*.txt'))
+        # gamryFiles = [f for f in fList if re.search(thisDate+'-'+'[0-9][0-9][0-9][0-9]_', f)]
+        nSweeps = np.size(gamryFiles)
+        meas = np.empty(nSweeps, dtype=object)
+        calStd = CalStdFit(interpMethod='cubic')
 
-    for i, file in enumerate(gamryFiles):
-        meas[i] = Solution(cmapName=cmapName)
-        print(i)
-        if i == 24:
-            x = 1
-        meas[i].loadFile(file, PAN=PAN_DATA)
+        lf = len(gamryFiles)
+        for i, file in enumerate(gamryFiles):
+            meas[i] = Solution(cmapName=cmapName)
+            print(f'measurement {i} of {lf}')
+            meas[i].loadFile(file, PAN=PAN_DATA)
 
-        if not np.isnan(meas[i].sigmaStd_Sm):
-            meas[i].sigmaStdCalc_Sm = calStd(meas[i].T_K, lbl_uScm=meas[i].lbl_uScm)
-        else:
-            meas[i].sigmaStdCalc_Sm = 1e-8  # Default air conductivity
-        meas[i].FitCircuit(circType=circType, initial_guess=initial_guess, PRINT=True,BASIN_HOPPING=False,MULTIPROC=True)
-        # meas[i].Kcell_pm = meas[i].sigmaStdCalc_Sm * meas[i].Rcalc_ohm
+            if not np.isnan(meas[i].sigmaStd_Sm):
+                meas[i].sigmaStdCalc_Sm = calStd(meas[i].T_K, lbl_uScm=meas[i].lbl_uScm)
+            else:
+                meas[i].sigmaStdCalc_Sm = 1e-8  # Default air conductivity
+            meas[i].FitCircuit(circType=circType, initial_guess=initial_guess, PRINT=False,BASIN_HOPPING=False,MULTIPROC=True)
 
-        if not PLOT_AIR and meas[i].comp == 'Air':
-            gamryFiles.remove(file)
-            cals.delete(cals[i])
-            add = '_noAir'
-    #     with open(file) as f:
-    #         f.readline()  # Skip intro line
-    #         meas[i].time = dtime.strptime(f.readline()[:-1], gamryDTstr)  # Measurement time
-    #         meas[i].T_K = readFloat(f.readline())  # Temp
-    #         meas[i].P_MPa = readFloat(f.readline())  # Pressure
-    #         meas[i].descrip = f.readline()  # Text description
-    #         meas[i].Vdrive_V = readFloat(f.readline())  # Driving voltage
-    #         meas[i].fStart_Hz = readFloat(f.readline())  # Driving voltage
-    #         meas[i].fStop_Hz = readFloat(f.readline())  # Driving voltage
-    #         meas[i].nfSteps = int(readFloat(f.readline()))  # Number of f steps
-    #
-    #     if 'DIwater' in meas[i].descrip:
-    #         meas[i].comp = 'PureH2O'
-    #         meas[i].sigmaStd_Sm = 0
-    #         meas[i].legLabel = r'$\approx0$'
-    #     elif 'Air' in meas[i].descrip:
-    #         meas[i].comp = 'Air'
-    #         meas[i].sigmaStd_Sm = np.nan
-    #         meas[i].legLabel = 'Air'
-    #     else:
-    #         meas[i].comp = 'KCl'
-    #         if '447uScm' in meas[i].descrip:
-    #             # meas[i].sigmaStd_Sm = float(meas[i].descrip.split(':')[-1].split('uScm')[0]) / 1e4
-    #             meas[i].sigmaStd_Sm = 447/1842 # 10kHz value from after-removal measurement on 20220923, phase at 10kHz is basically zero.
-    #             meas[i].legLabel = '447uScm KCl std'
-    #
-    #         elif '2764uScm' in meas[i].descrip:
-    #             meas[i].sigmaStd_Sm = 2764/614 # 10kHz value from after-removal measurement on 20220923, phase at 10kHz is basically zero.
-    #             meas[i].legLabel = '2764uScm KCl std'
-    #
-    #         elif '8Sm' in meas[i].descrip:
-    #             meas[i].sigmaStd_Sm = 80000 / 28  # 10kHz value from after-removal measurement on 20220923, phase at 10kHz is basically zero.
-    #             meas[i].legLabel = '8Sm KCl std'
+        allMeas[d_ind] = meas
 
-        # _, meas[i].f_Hz, Zabs_ohm, Phi_ohm = np.loadtxt(file, skiprows=10, unpack=True)
-        # # Phase negated for convenience looking at plots in 1st quadrant
-        # meas[i].Z_ohm = Zabs_ohm * np.exp(-1j*np.deg2rad(Phi_ohm))
-        # # meas[i].sigma_Sm = np.real(np.interp(10e3,meas[i].f_Hz,meas[i].Z_ohm))*meas[i].sigmaStd_Sm)
-        # # print(i)
-        # meas[i].sigma_Sm = np.real(np.interp(10e3,meas[i].f_Hz, Zabs_ohm)) * meas[i].sigmaStd_Sm * 1e-4
-        # circuit.fit(meas[i].f_Hz,meas[i].Z_ohm)
-        # meas[i].R0 = circuit.parameters_[0]
-        # meas[i].Z_fit = circuit.predict(meas[i].f_Hz)
-        # meas[i].sigma_Sm = meas[i].Kcell_pm/meas[i].Rcalc_ohm
+    timeseries = TimeSeries(allMeas)
+    timeseries.organizeData()
+    ds = DataSelector(timeseries)
+    ds.show()
 
+    # Check if the main window is visible
+    if not ds.isVisible():
+        print("The window is not visible")
 
-    listStandards = np.array([cal.sigmaStd_Sm for cal in meas])
-    uniqueListStandards = np.unique(listStandards)
-    # standardsSort = np.argsort(uniqueListStandards)
-    # for cal in meas:
-    #     colorLabel = standardsSort[cal.sigmaStd_Sm == uniqueListStandards][0]
-    #     cal.color = cmap(colorLabel / np.max(standardsSort))
-    #
-    #
-    # # Sort list based on conductivity
-    # iSort = np.argsort(listStandards)
-    # meas = meas[iSort]
+    # The geometry of the window can be printed to ensure it's within the visible area of your screen
+    print("Window geometry:", ds.geometry())
 
-    allMeas[d_ind] = meas
+    # Start the event loop
+    return_code = app.exec_()
+    print("Event loop exited with code:", return_code)
+    sys.exit(return_code)
 
-PlotSigma(allMeas,figSize,outFigName,xtn)
-
+# PlotTimeseries(allMeas,figSize,outFigName,xtn)
+if __name__ == "__main__":
+    main()
 # 1/Z_cell = 1/R + i*omega*C -- Pan et al. (2021): https://doi.org/10.1029/2021GL094020
